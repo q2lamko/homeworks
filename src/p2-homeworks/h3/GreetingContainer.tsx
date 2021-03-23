b5ebcdb45f4841dfabb1d0677ab7c1604c736441
q2lamko <Avenger_adamant@mail.ru>
2021-03-23 19:28:26 +0700

    new homework

--- src/p2-homeworks/h3/GreetingContainer.tsx
+++ src/p2-homeworks/h3/GreetingContainer.tsx
@@ -3,14 +3,10 @@ import Greeting from "./Greeting";
 
 type GreetingContainerPropsType = {
     users: any // need to fix any
-    addUserCallback: any // need to fix any
+    addUserCallback: (name: string) => void // need to fix any
 }
 
-// более простой и понятный для новичков
-// function GreetingContainer(props: GreetingPropsType) {
 
-// более современный и удобный для про :)
-// уровень локальной логики
 const GreetingContainer: React.FC<GreetingContainerPropsType> = ({users, addUserCallback}) => { // деструктуризация пропсов
     const [name, setName] = useState<any>(""); // need to fix any
     const [error, setError] = useState<any>(""); // need to fix any
@@ -19,7 +15,8 @@ const GreetingContainer: React.FC<GreetingContainerPropsType> = ({users, addUser
         setName(""); // need to fix
     };
     const addUser = () => {
-        alert(`Hello  !`); // need to fix
+        addUserCallback(users)
+        // need to fix
     };
 
     const totalUsers = 0; // need to fix
